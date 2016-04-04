fc1addca17d39ad617cfa74cd9bded4821f457b2
BaptisteGaitet <baptiste.gaitet@gmail.com>
2016-04-05 00:33:28 +0200

    Window size tracking

---
+++ window.js
@@ -0,0 +1,16 @@
+function init()
+{
+	screenWidth = window.innerWidth;
+	screenHeight = window.innerHeight;
+}
+
+function onResize()
+{
+	screenWidth = window.innerWidth;
+	screenHeight = window.innerHeight;
+}
+
+var screenWidth = 0;
+var screenHeight = 0;
+
+init();
